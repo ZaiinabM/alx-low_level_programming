176f475ab5e6b06d6fb89d6636b642100fcf7683
Zainab Mai <zainabmustaphamai@gmail.com>
2022-10-14 16:24:53 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'Z'; ch => 'A'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	Return(0);
