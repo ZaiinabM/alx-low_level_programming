4d2ac24af0a5e55906e451a39b13edb43d2b8fea
Zainab Mai <zainabmustaphamai@gmail.com>
2022-10-14 18:12:39 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main- entry
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+	char x;
+
+	for (n = '0'; n <= '10'; n++)
+	{
+		putchar(n);
+	}
+	for (x = 'a'; n <= 'f'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
