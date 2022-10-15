0f0693e76323d16d6f1b9c038995b961c0137da4
Zainab Mai <zainabmustaphamai@gmail.com>
2022-10-14 17:25:55 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- entry
+ * Return: 0 if no error
+ */
+int main(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
