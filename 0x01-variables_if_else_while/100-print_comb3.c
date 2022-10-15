64def84cec740b1aa929a43d344c8ce5b89d4b01
Zainab Mai <zainabmustaphamai@gmail.com>
2022-10-15 11:34:20 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main- possible combinations of two digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < 10; n1++)
+	{
+		for (n2 = 0; n2 < 10; n2++)
+		{
+			putchar((n1 % 10) + '0');
+			putchar((n2 % 10) + '0');
+
+			if (n1 == 9 && n2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
