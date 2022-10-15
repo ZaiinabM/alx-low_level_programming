fbd5160627cbfce3dfa866049c898871a9de282f
Zainab Mai <zainabmustaphamai@gmail.com>
2022-10-15 12:50:58 -0700

    100-print_comb3.correction

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,21 +5,32 @@
  */
 int main(void)
 {
-	int n1, n2;
+	int n1, n2 = '0', n3;
 
-	for (n1 = 0; n1 < 10; n1++)
+	for (n1 = 0; n1 <= 9; n1++)
 	{
-		for (n2 = 0; n2 < 10; n2++)
+		for (n2 = n3; n2 <= 9; n2++)
 		{
-			putchar((n1 % 10) + '0');
-			putchar((n2 % 10) + '0');
-
-			if (n1 == 9 && n2 == 9)
+			if (n1 != n3)
+			{
+				putchar(n1);
+				putchar(n3);
+			}
+			if (n1 == n3)
+			{
 				continue;
-
-			putchar(',');
-			putchar(' ');
+			}
+			if (n1 == 8 && n3 == 9)
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
+		n2++;
 	}
 	putchar('\n');
 	return (0);
