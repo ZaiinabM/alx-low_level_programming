bf7ce526a0866e3af72b20f7dcb65f5205c9d1b9
Zainab Mai <zainabmustaphamai@gmail.com>
2022-10-14 17:45:56 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- entry
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x => 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
