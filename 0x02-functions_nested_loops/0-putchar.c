2b99fbd657c584d111e3fe8800f336b962251b31
Zainab Mai <zainabmustaphamai@gmail.com>
2022-10-18 07:59:04 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main- print _putchar
+ * Return: 0 Always
+ */
+int main(void)
+{
+	_putchar ch[] = {_, p, u, t, c, h, a, r};
+	{
+		putchar("%s", ch);
+	}
+	putchar("\n");
+	return (0);
+}
