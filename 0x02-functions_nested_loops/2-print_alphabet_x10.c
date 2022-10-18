955f2f72dba14c7dedc91738b56dca6c0651a934
Zainab Mai <zainabmustaphamai@gmail.com>
2022-10-18 09:37:22 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- prints the alphabets 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	char i;
+
+	for (n = 0; n < 10; n++)
+	{
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+	}
+}
