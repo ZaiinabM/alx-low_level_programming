2d9bc25393b1f31f4e8cc7e2020ef07e65d4398c
Zainab Mai <zainabmustaphamai@gmail.com>
2022-10-18 09:20:01 -0700

    lowercase alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet- prints alphabets in lowercase
+ */
+void print_alphabet(void)
+{
+	char le;
+
+	for (le = 'a'; le <= 'z'; le++)
+		_putchar(le);
+	_putchar('\n');
+}
