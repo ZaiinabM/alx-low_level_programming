8e01397a22320e8fa29c9ea4ab1c903ff8b4cf3e
Zainab Mai <zainabmustaphamai@gmail.com>
2022-10-20 17:30:54 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_most_numbers - print numbers 0-9, excluding 2 and 4
+ * print_most_numbers - print numbers 0 to 9, excluding 2 and 4
  * Return: void
  */
 void print_most_numbers(void)
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,5 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the number, from 0 to 14
+ */ 
+void more_numbers(void)
