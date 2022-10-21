182d02a35cd887ef86b0d75d2a6234fad7de78ef
Zainab Mai <zainabmustaphamai@gmail.com>
2022-10-21 13:51:05 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints the largest prime number
+ * Return: 0 Always
+ */
+int main(void)
+{
+	long int n, xy;
+
+	n = 612852475143;
+	for (xy = 2; xy <= n; xy++)
+	{
+		if (n % xy == 0)
+		{
+			n /= xy;
+			xy--;
+		}
+	}
+	printf("Id\n", xy);
+	return (0);
+}
+
