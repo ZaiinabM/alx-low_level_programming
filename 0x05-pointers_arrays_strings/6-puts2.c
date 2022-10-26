09e4874a781b6da442f6f5a69bbd5bd0b5f277fe
Zainab Mai <zainabmustaphamai@gmail.com>
2022-10-25 17:12:40 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: the string input
+ */
+void puts2(char *str)
+{
+
+	int n = 0;
+
+	while (n >= 0)
+	{
+		if (str[n] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (n % 2 == 0)
+			_putchar(str[n]);
+		n++;
+	}
+}
