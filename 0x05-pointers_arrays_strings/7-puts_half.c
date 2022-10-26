8f81c03bf698df97d743047543f1f6c800c89a6c
Zainab Mai <zainabmustaphamai@gmail.com>
2022-10-25 17:35:04 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: input string
+ */
+void puts_half(char *str)
+{
+	int i, n = 0;
+
+	while (n >= 0)
+	{
+	if (str[n] == '\0')
+		break;
+	n++;
+	}
+	if (n % 2 == 1)
+		i = n / 2;
+	else
+		i = (n - 1) / 2;
+
+	for (i++; i < n; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
