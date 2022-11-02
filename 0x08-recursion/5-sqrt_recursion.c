ab995af2d602aa0ab5bcfbe694688af2f35c04e0
Zainab Mai <zainabmustaphamai@gmail.com>
2022-11-02 12:54:41 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,22 @@
 #include "main.h"
+
+/**
+ * power_operation - returns the natura sqr rt f a num
+ * @n: input
+ * @c: iteratr
+ * Return: sqr rt r -1
+ */
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+			return (0 + power_operation(n, c + 1));
+}
 /**
  * _sqrt_recursion - prints sqr root of a num
  * @n: the num
@@ -12,5 +30,5 @@ int _sqrt_recursion(int n)
 	return (0);
 	if (n == 1)
 		return (1);
-	return (x * _pow_recursion(x, y - 1));
+	return (power_operation(n, 2));
 }
