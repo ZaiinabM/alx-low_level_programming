5ca4bf4a9ec515b6e9ba34dfe60cc9aef416eb83
Zainab Mai <zainabmustaphamai@gmail.com>
2022-11-05 11:37:29 -0700

    fix fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,14 +8,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	(void)argv;
 
-	if (argc == 1)
 	printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-		printf("%d\n", i - 1);
-	}
-	return (0);
+	exit(EXIT_SUCCESS);
 }
