705eae259c902b27e42bf35e5a04d720b48143c7
Zainab Mai <zainabmustaphamai@gmail.com>
2022-11-05 11:18:06 -0700

    print num f args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints program name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 Always
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+		return (0);
+}
