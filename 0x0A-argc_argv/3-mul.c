04ba16c08322d3d6191dab5df103adbae60b0722
Zainab Mai <zainabmustaphamai@gmail.com>
2022-11-05 12:05:40 -0700

    3-mul. fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,8 +13,7 @@ int main(int argc, char *argv[])
 	if (argc == 3)
 	{
 		for (i = 1; i < argc; i++)
-			prduct = prduct *
-				strtol(argv[i], NULL, 10);
+			prduct = prduct * strtol(argv[i], NULL, 10);
 		printf("%d\n", prduct);
 	}
 	else
