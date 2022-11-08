91a54e026f2a8ff540bab70b4f98d6dec39c1e19
Zainab Mai <zainabmustaphamai@gmail.com>
2022-11-08 13:02:49 -0800

    fix concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,6 +29,7 @@ char *str_concat(char *s1, char *s2)
 			s3[i] = s1[i];
 			i++;
 		}
+	}
 		if (s2)
 		{
 			while (i < (len1 + len2))
@@ -40,5 +41,4 @@ char *str_concat(char *s1, char *s2)
 		}
 		s3[i] = '\0';
 		return (s3);
-}
 }
