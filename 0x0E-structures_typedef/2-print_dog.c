4e3b06ed325b0e8761804ecfb10fd58e88aa4d7d
Zainab Mai <zainabmustaphamai@gmail.com>
2022-11-15 12:15:42 -0800

    variable init

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - prints a dog struct
+ * @d: dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ * Return: no return
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d != NULL)
+	{
+		d.name = name;
+		d.age = age;
+		d.owner = owner;
+	}
+}
